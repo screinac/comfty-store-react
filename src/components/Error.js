f1f2746eab86c09dd1003d60c7025a6926b919b2
Sergio <sreina@unal.edu.co>
2021-10-12 08:23:46 -0500

    initial commit

---
+++ src/components/Error.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const Error = () => {
+  return (
+    <div className='section-center section'>
+      <h2>There was an error...</h2>
+    </div>
+  )
+}
+
+export default Error
